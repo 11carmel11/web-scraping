8e4430138cebbe417b49af83f20ad3598a15aa5d
11carmel11 <11carmrl11@gmail.com>
2022-02-06 20:48:55 +0200

    converted to generic scraper

--- back/helpers.py
+++ back/helpers.py
@@ -1,5 +1,7 @@
 import requests
 
+empty = {"title": "UNKNOWN TITLE", "author": "ANONYMOUS AUTHOR"}
+
 proxies = {
     "http": "socks5h://127.0.0.1:9050",
     "https": "socks5h://127.0.0.1:9050",
--- back/main.py
+++ back/main.py
@@ -1,22 +1,13 @@
 # command curl --proxy socks5h://127.0.0.1:9050 http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all
-from helpers import get
+from helpers import get, empty
 from bs4 import *
 
-empty = {"title": "UNKNOWN TITLE", "author": "ANONYMOUS AUTHOR"}
-db = []
 
 url = "http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all"
 
-home_res = get(url)
 
-html = BeautifulSoup(home_res, "html.parser")
-
-
-pastes_list = html.find("section")
-
-
-def scrape_title(paste):
-    title = paste.find("h4")
+def scrape_title(paste, title_tag):
+    title = paste.find(title_tag)  # h4
 
     if hasattr(title, "text"):
         return title.text.strip()
@@ -24,16 +15,16 @@ def scrape_title(paste):
         return empty["title"]
 
 
-def scrape_content(paste):
-    ol_list = paste.find("ol")
+def scrape_content(paste, content_tag):
+    content_list = paste.find(content_tag)  # ol
 
-    if not hasattr(ol_list, "text"):
+    if not hasattr(content_list, "text"):
         # paste must have context!
         return None
 
     content = ""
     i = 1
-    for text in ol_list:
+    for text in content_list:
         text = text.text
         stripped = text.strip()
         if text == stripped:
@@ -43,8 +34,8 @@ def scrape_content(paste):
     return content
 
 
-def scrape_author_and_date(paste):
-    closure_data = paste.find("div", {"class": "col-sm-6"}).text
+def scrape_author_and_date(paste, data_tag, data_class):
+    closure_data = paste.find(data_tag, {"class": data_class}).text  # div, col-sm-6
     splitted_data = closure_data.strip().split(" ")
 
     author = splitted_data[2] if bool(splitted_data[2]) else empty["author"]
@@ -53,18 +44,38 @@ def scrape_author_and_date(paste):
     return [author, date]
 
 
-for paste in pastes_list:
-    # title section
-    title = scrape_title(paste)
+def scrape(url, title_tag, content_tag, data_tag, data_class):
+    db = []
 
-    # content section
-    content = scrape_content(paste)
-    if not content:
-        continue
+    home_res = get(url)
 
-    # author and date section
-    author, date = scrape_author_and_date(paste)
+    html = BeautifulSoup(home_res, "html.parser")
 
-    # inserting to db
-    final_data = {"author": author, "date": date, "content": content, "title": title}
-    db.append(final_data)
+    pastes_list = html.find("section")
+
+    for paste in pastes_list:
+        # title section
+        title = scrape_title(paste, title_tag)
+
+        # content section
+        content = scrape_content(paste, content_tag)
+        if not content:
+            continue
+
+        # author and date section
+        author, date = scrape_author_and_date(paste, data_tag, data_class)
+
+        # inserting to db
+        final_data = {
+            "author": author,
+            "date": date,
+            "content": content,
+            "title": title,
+        }
+        db.append(final_data)
+
+    return db
+
+
+test_db = scrape(url, "h4", "ol", "div", "col-sm-6")
+print(len(test_db))
