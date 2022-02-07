75d4ee1aba1725613c9d5f2550f8831fceea46fa
11carmel11 <11carmrl11@gmail.com>
2022-02-07 20:13:49 +0200

    adjusted back to use sse

--- back/main.py
+++ back/main.py
@@ -1,13 +1,18 @@
 # command curl --proxy socks5h://127.0.0.1:9050 http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all
 from helpers import scrape
-from config import url, port
+from config import port
 
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_cors import CORS, cross_origin
+from functools import partial
+from time import sleep
 import json
+
 
 app = Flask(__name__)
 CORS(app)
+
+partial_scrappers = {}
 
 
 @app.route("/")
@@ -15,17 +20,36 @@ def index():
     return "hello from index"
 
 
-@app.route("/scrape", methods=["POST"])
+@app.route("/set/data", methods=["POST"])
 @cross_origin()
-def post_scrape_by_request():
+def set_scrape_data():
     body = request.json
     url = body.get("url")
     pastes_list_info = body.get("pastesListInfo")
     title_info = body.get("titleInfo")
     content_info = body.get("contentInfo")
     data_info = body.get("dataInfo")
-    scrape_db = scrape(url, pastes_list_info, title_info, content_info, data_info)
-    return json.dumps(scrape_db)
+
+    partial_scrappers[url] = partial(
+        scrape, url, pastes_list_info, title_info, content_info, data_info
+    )
+
+    return "data set"
+
+
+@app.route("/stream")
+def stream():
+    url = request.args.get("url")
+
+    def fetch_data():
+        while True:
+            relevant_scraper = partial_scrappers[url]
+            scrape_db = relevant_scraper()
+            data = json.dumps(scrape_db)
+            yield f"data: {data} \n\n"
+            sleep(5)
+
+    return Response(fetch_data(), mimetype="text/event-stream")
 
 
 if __name__ == "__main__":
